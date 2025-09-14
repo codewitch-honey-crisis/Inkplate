ef71270aeb9305940a32ddc74dd6637f1dde0f4b
codewitch-honey-crisis <gaztoast@gmail.com>
2025-09-14 05:56:59 -0700

    reduced log output

--- src/esp32/main.c
+++ src/esp32/main.c
@@ -2,9 +2,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "rtc_wdt.h"
+#include "esp_log.h"
 extern void run(void);
 extern void loop(void);
 void app_main(void) {
+    esp_log_level_set("*", ESP_LOG_NONE);
     run();
     uint32_t ts = 0;
     while (1) {
