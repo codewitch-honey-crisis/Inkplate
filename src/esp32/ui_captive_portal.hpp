32b4a047026bfd6a68454e8fcd0da5129dd5f0de
Andy Walker <andy@andy.dev>
2025-09-28 22:25:49 -0400

    remove state tracking/update during config
    - No need to burn a screen refresh on wifi connect to AP just to update the string. Static screen is enough

--- src/esp32/ui_captive_portal.hpp
+++ src/esp32/ui_captive_portal.hpp
@@ -7,8 +7,5 @@ bool ui_captive_portal_init();
 /// @brief Sets the initial AP connection screen of the captive portal UI
 /// @return True on success, otherwise false
 bool ui_captive_portal_set_ap(const char* address,const char* ssid, const char* pass);
-/// @brief Sets the configuration screen of the captive portal UI
-/// @return True on success, otherwise false
-bool ui_captive_portal_set_configure(const char* address);
 /// @brief Deinitializes the captive portal UI
 void ui_captive_portal_end();
